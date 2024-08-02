3d2474fa2fec735a2e12784ab37afb5b0d1fe2b5
eMKa18 <1918102+eMKa18@users.noreply.github.com>
2024-08-02 16:21:49 +0200

    mobile design added, not yest perfect

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -12,34 +12,41 @@ export default function Index() {
     <div className="lg:w-[80%] md:w-[90%] w-full flex flex-col items-center mobile">
       <div className="flex justify-center w-full pt-10 pb-10">
         <nav className="flex justify-between items-center w-full">
-          <div className="">
+          <div>
             <img src="./shared/desktop/logo-dark.png" alt="Logo" />
           </div>
-          <div className="flex items-center justify-between w-1/4">
-            <div className="">Our company</div>
-            <div className="">Locations</div>
-            <div className="">Contact</div>
+          <div className="flex items-center justify-between w-1/4 uppercase text-sm tracking-wider">
+            <div>Our company</div>
+            <div>Locations</div>
+            <div>Contact</div>
           </div>
         </nav>
       </div>
       <div className="w-full bg-accent rounded-xl grid xl:grid-cols-3 relative xl:min-h-[700px] bg-hero-pattern-home bg-no-repeat xl:bg-right xl:bg-contain bg-auto">
-        <div className="font-sans font-bold text-white flex flex-col justify-around lg:items-start items-center xl:p-[100px] md:p-24 p-16 xl:col-span-2">
-          <h1 className="text-left lg:text-7xl text-4xl text-wrap xl:-mb-4.5 lg:mb-2 mb-4">
+        <div className="font-sans font-bold text-white flex flex-col justify-around lg:items-start items-center xl:p-[100px] md:p-24 p-16 xl:col-span-2 text-center xl:text-left">
+          <h1 className="lg:text-7xl text-4xl xl:mb-4.5 lg:mb-2 mb-4">
             Award-winning custom designs and digital branding solutions
           </h1>
-          <p className="text-left text-wrap lg:pr-[150px] xl:-mb-4.5 lg:mb-2 mb-4">
+          <p className="lg:pr-[150px] xl:mb-4.5 lg:mb-2 mb-4">
             With over 10 years in the industry, we are experienced in creating
             fully responsive websites, app design, and engaging brand
             experiences. Find out more about our services.
           </p>
-          <button className="bg-white text-black xl:rounded-md rounded py-4 px-8">
+          <button className="bg-white text-black xl:rounded-md rounded py-4 px-8 mb-10">
             Learn more
           </button>
+          <div className="relative w-full flex justify-center items-center mt-10 xl:hidden">
+            <img
+              className="scale-75 md:scale-100 lg:scale-125"
+              src="./home/desktop/image-hero-phone.png"
+              alt="Hero phone"
+            />
+          </div>
         </div>
-        <div className="relative justify-center items-center overflow-visible">
+        <div className="relative flex justify-center items-center overflow-hidden hidden xl:flex xl:col-span-1">
           <div className="h-full w-full rounded-xl overflow-hidden relative">
             <img
-              className="absolute bottom-[-300px] left-[90px] xl:scale-150 origin-bottom-right scale-.5"
+              className="absolute bottom-0 left-1/2 transform -translate-x-1/2 scale-75 md:scale-100 lg:scale-125"
               src="./home/desktop/image-hero-phone.png"
               alt="Hero phone"
             />
