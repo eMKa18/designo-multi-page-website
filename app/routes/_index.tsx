19883d90425464e79f8be3749113779e863a961f
eMKa18 <1918102+eMKa18@users.noreply.github.com>
2024-08-02 11:59:12 +0200

    Adding navbar and first part of the home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -23,20 +23,21 @@ export default function Index() {
           </div>
         </nav>
       </div>
-      <div className="w-full bg-red-400 rounded-md border border-red-400 flex flex-row">
-        <h1>Award-wining custom designs and digital branding solutions</h1>
-        <p>
-          With over 10 years in the industry, we are experienced in creating
-          fully responsive websites, app design, and engaging brand experiences.
-          Find out more about our services.
-        </p>
-        <img src="/public/home/desktop/image-hero-phone.png" alt="Hero phone" />
-        {/* CSS as a background? */}
-        <img
-          src="/public/home/desktop/bg-pattern-hero-home.svg"
-          alt="Background"
-        />
-        <button>Learn more</button>
+      <div className="w-full bg-red-400 rounded-md border border-red-400 grid grid-cols-2">
+        <div className="font-sans font-bold text-white flex">
+          <h1>Award-wining custom designs and digital branding solutions</h1>
+          <p>
+            With over 10 years in the industry, we are experienced in creating
+            fully responsive websites, app design, and engaging brand
+            experiences. Find out more about our services.
+          </p>
+          <button>Learn more</button>
+        </div>
+        <div>
+          <img src="/home/desktop/image-hero-phone.png" alt="Hero phone" />
+          {/* CSS as a background? */}
+          <img src="/home/desktop/bg-pattern-hero-home.svg" alt="Background" />
+        </div>
       </div>
     </div>
   );
