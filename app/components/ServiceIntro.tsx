407138371f6f3175503c72d01bc25d30cbc0295d
eMKa18 <1918102+eMKa18@users.noreply.github.com>
2024-08-06 13:28:36 +0200

    Making description taking less space

--- app/components/ServiceIntro.tsx
+++ app/components/ServiceIntro.tsx
@@ -8,7 +8,7 @@ const ServiceIntro = ({
   return (
     <div className="bg-accent rounded-xl text-center text-white p-20 bg-hero-pattern-intro-web bg-no-repeat bg-right mb-8">
       <h1 className="text-4xl sm:text-3xl font-bold mb-8">{title}</h1>
-      <p className="w-[50%] mx-auto">{description}</p>
+      <h2 className="w-[50%] mx-auto">{description}</h2>
     </div>
   );
 };
